663098a5643025b548b2a747fc9246a99220ef43
孙佳琦 <laosunjiaqi@126.com>
2020-06-30 20:23:24 +0800

    首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,173 @@
 <template>
   <div class="home">
-  
+    <div class="home_header">
+      <div class="home_header_swiper">
+        <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+          <van-swipe-item v-for="(item,index) in swiperItem" :key="index">
+            <img :src="item.swiper_icon" />
+          </van-swipe-item>
+        </van-swipe>
+      </div>
+    </div>
+    <div class="home_body">
+      <div class="home_body_header">
+        <div class="home_body_header_item" v-for="(item,index) in bodyHeaderItem" :key="index">
+          <div class="home_body_header_div">
+            <div class="home_body_header_img">
+              <img :src="item.body_image" />
+            </div>
+          </div>
+          <div class="home_body_header_sone">
+            <p>{{item.body_name}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="body_contet">
+        <div class="body_content_item" v-for="(item,index) in teacherItem" :key="index">
+          <div class="teacherItem">
+            <p>{{item.content_tiitle}}</p>
+          </div>
+          <div class="teacherItem_list" v-for="(item,index) in item.content_item" :key="index">
+            <div class="teacherItem_image">
+              <img :src="item.head_portrait" />
+            </div>
+            <div class="teacherItem_title">
+              <div class="teacherItem_name">{{item.name}}</div>
+              <div class="teacherItem_introduce">{{item.introduce}}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
+  components: {},
+  data() {
+    return {
+      swiperItem: [],
+      bodyHeaderItem: [],
+      teacherItem: []
+    };
+  },
+  methods: {},
+  mounted() {
+    axios.get("http://localhost:8080/home.json").then(response => {
+      this.swiperItem = response.data.swiper;
+      this.bodyHeaderItem = response.data.body_icon;
+      this.teacherItem = response.data.body_content;
+    });
   }
-}
+};
 </script>
+
+<style scoped>
+.home {
+  background-color: #f0f2f5;
+}
+.home_header {
+  width: 100%;
+  height: 200px;
+}
+.my-swipe {
+  width: 100%;
+  height: 200px;
+}
+.van-swipe-item img {
+  width: 100%;
+}
+.home_body {
+  width: 100%;
+  height: 500px;
+}
+.home_body_header {
+  width: 95%;
+  height: 103px;
+  padding: 0 2.5%;
+  position: relative;
+  bottom: 20px;
+  display: inline-flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+.home_body_header_item {
+  width: 29%;
+  height: 103px;
+  background: #fff;
+  border-radius: 15px;
+}
+.home_body_header_div {
+  width: 100%;
+  height: 50px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+.home_body_header_img {
+  width: 20%;
+  height: 21px;
+}
+.home_body_header_img img {
+  width: 100%;
+}
+.home_body_header_sone {
+  width: 100%;
+  display: inline-flex;
+  justify-content: center;
+  color: #8c8c8c;
+  font-size: 13px;
+}
+.body_contet {
+  width: 95%;
+  padding: 0 2.5%;
+}
+.body_content_item {
+  width: 100%;
+}
+.teacherItem {
+  width: 100%;
+  height: 20px;
+  border-left: 2px solid orangered;
+}
+.teacherItem p {
+  padding-left: 20px;
+}
+.teacherItem_list {
+  width: 91%;
+  height: 81px;
+  padding: 0 4.5%;
+  margin-bottom: 10px;
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+}
+.teacherItem_image {
+  width: 13%;
+  height: 40px;
+  border-radius: 20px;
+  overflow: hidden;
+}
+.teacherItem_image img {
+  width: 100%;
+  height: 100%;
+}
+.teacherItem_title {
+  width: 77%;
+  height: 40px;
+  padding-left: 10%;
+  overflow: hidden;
+}
+.teacherItem_name {
+  font-size: 14px;
+}
+.teacherItem_introduce {
+  width: 100%;
+  font-size: 14px;
+  color: #b7b7b7;
+}
+</style>
