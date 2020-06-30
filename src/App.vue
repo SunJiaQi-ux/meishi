4de76e7d1893f5090ba7e9d8def22869687ba53a
孙佳琦 <laosunjiaqi@126.com>
2020-06-30 18:07:16 +0800

    第一次

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <van-tabbar v-model="active" route>
+      <van-tabbar-item replace icon="home-o" to="/">首页</van-tabbar-item>
+      <van-tabbar-item replace icon="search" to="">课程</van-tabbar-item>
+      <van-tabbar-item replace icon="friends-o" to >约课记录</van-tabbar-item>
+      <van-tabbar-item replace icon="setting-o" to >练习</van-tabbar-item>
+      <van-tabbar-item replace icon="setting-o" to >我的</van-tabbar-item>
+    </van-tabbar>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
