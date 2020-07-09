209823c40270c04c38f10e17e41a0e3b05dc6366
Mr159 <1594341780@qq.com>
2020-07-09 15:39:44 +0800

    img

--- src/views/mym/Mstudy.vue
+++ src/views/mym/Mstudy.vue
@@ -6,7 +6,10 @@
             <img src="../../assets/mym_img/fan.jpg" alt="">
         </div>
         <van-tabs v-model="active">
-        <van-tab title="直播课(0)">内容 1</van-tab>
+        <van-tab title="直播课(0)">
+            <img class="lu-img" src="../../assets/mym_img/lu.jpg" alt="">
+            <button class="select" @click="kecheng">选择课程</button>
+        </van-tab>
         <van-tab title="点播课(0)">内容 2</van-tab>
         <van-tab title="音频课(0)">内容 3</van-tab>
         <van-tab title="图文课(0)">内容 4</van-tab>
@@ -28,6 +31,11 @@ export default {
     methods:{
         goBack(){
             window.history.go(-1)
+        },
+        kecheng(){
+            this.$router.push({
+                path:"/course"
+            })
         }
     }
 }
@@ -48,5 +56,19 @@ export default {
 }
 .head p{
     font-size: 0.3rem;
+}
+.lu-img{
+    width: 2.49rem;
+    margin: 3rem 2.5rem 0 2.5rem;
+}
+.select{
+    width: 2rem;
+    height: 0.5rem;
+    margin: 1rem 2.5rem 0 2.7rem;
+    font-size: 0.16rem;
+    background: #E96201;
+    color: #fff;
+    outline: none;
+    border: none;
 }
 </style>
